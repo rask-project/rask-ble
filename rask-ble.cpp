fd8639133abf78e928f72736e034d88dd3f644f8
Mauro Marssola <mauro.marssola@hotmail.com>
2021-03-18 10:33:30 -0300

    rask-ble as shared library

--- rask-ble.cpp
+++ rask-ble.cpp
@@ -2,21 +2,21 @@
 
 #include <iostream>
 
-#include <esp_bt.h>
-#include <esp_gap_ble_api.h>
-#include <esp_gattc_api.h>
-#include <esp_gatt_defs.h>
-#include <esp_bt_main.h>
-#include <esp_gatt_common_api.h>
-#include <esp_log.h>
+// #include <esp_bt.h>
+// #include <esp_gap_ble_api.h>
+// #include <esp_gattc_api.h>
+// #include <esp_gatt_defs.h>
+// #include <esp_bt_main.h>
+// #include <esp_gatt_common_api.h>
+// #include <esp_log.h>
 
 namespace Rask {
 namespace BLE {
 
 BLEController::BLEController()
 {
-    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    // ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    // esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
 }
 
 } // namespace BLE
